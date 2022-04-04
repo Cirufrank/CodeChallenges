fbd883a844d4a82a90310b822360f0c9ee8a57b1
Cirũ Franklin <cirufrank@me.com>
2022-04-03 18:31:33 -0700

    Complete daily code challenege and begin using OOP best practices to complete push work calculator

---
+++ Calculator_Push_Work/main.js
@@ -0,0 +1,17 @@
+function makeCalculator(fontSize, textColor, mainColor) {
+    this.fontSize = fontSize;
+    this.textColor = textColor;
+    this.mainColor = mainColor;
+    this.makeCalculation = function() {
+        /* store input and return result */
+        console.log("calculation");
+    }
+    this.displayCalculation = function() {
+        /* Display input once it has been stored */
+        console.log("display caclulation");
+    }
+    this.displayResult = function() {
+        /* Display result */
+        console.log("Display Result")
+    }
+}
