7e7c518b92058b10cc01265b3044a55c5c5cac71
Cirũ Franklin <cirufrank@me.com>
2022-04-25 21:24:15 -0700

    Add daily code challenege of total positive integers greater than 0 that are odd and less than the number passed into the function

---
+++ Code_Challeneges_Week_6/oddNumsBelowNum.js
@@ -0,0 +1,39 @@
+/*
+
+Instructions:
+
+Given a number n, return the number of positive odd numbers below n, EASY!
+
+Examples (Input -> Output)
+* 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+* 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+Expect large Inputs!
+
+*/
+
+function oddCount(n){
+    let resultArray = [];
+    for (let i = n - 1; i > 0; i -= 1) {
+      if (i % 2 !== 0) resultArray.push(i)
+    }
+    
+    return resultArray.length;
+  }
+
+  /*
+  
+  Tests:
+  
+  const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+   it("Testing for fixed tests", () => {
+    assert.strictEqual(oddCount(15), 7, "Oops! Wrong.");
+    assert.strictEqual(oddCount(15023), 7511, "Oops! Wrong.");
+  });
+});
+
+
+*/
