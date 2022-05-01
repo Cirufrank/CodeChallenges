3dcb1462cfa7124e95619f3355722e72416df89c
Cirũ Franklin <cirufrank@me.com>
2022-04-30 21:43:34 -0700

    Update function to return array with one element when the array passed into the function only has one element

--- Code_Challeneges_Week_7/fullNumsArray.js
+++ Code_Challeneges_Week_7/fullNumsArray.js
@@ -18,6 +18,7 @@ Input: 1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
 */
 
 function pipeFix(numbers){
+    if (numbers.length === 1) return numbers;
     let endOfNums = numbers.pop();
     let beginningOfNums = numbers.shift();
     let numArray = [];
