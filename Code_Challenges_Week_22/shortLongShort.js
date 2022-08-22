f01c18a8549d828ded64ebd9cfeec047a95d1198
Cirũ Franklin <cirufrank@me.com>
2022-08-21 18:48:29 -0700

    Add daily code challenge of function that takes in two strings and returns a concatenation of the shortest string, longest string, and shortest string again

---
+++ Code_Challenges_Week_22/shortLongShort.js
@@ -0,0 +1,36 @@
+/*
+
+Instructions:
+
+Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+
+Hint for R users:
+
+The length of string is not always the same as the number of characters
+For example: (Input1, Input2) --> output
+
+("1", "22") --> "1221"
+("22", "1") --> "1221"
+
+*/
+
+function solution(a, b){
+    const [shortString, longString] = ( a.length < b.length ) ? [a, b] : [b , a];
+    return `${shortString}${longString}${shortString}`
+  }
+
+  
+/*
+
+Tests:
+
+describe("Sample tests", function() {
+  it("Should pass sample tests", function() {
+    Test.assertEquals(solution('45', '1'), '1451');
+    Test.assertEquals(solution('13', '200'), '1320013');
+    Test.assertEquals(solution('Soon', 'Me'), 'MeSoonMe');
+    Test.assertEquals(solution('U', 'False'), 'UFalseU');
+  });
+});
+    
+*/
