79952e77ab1fb982fb6c71fbfff25ac39cd20d48
Cirũ Franklin <cirufrank@me.com>
2022-09-05 19:19:55 -0700

    Add daily code challenge of function that takes in a string and changes all a's to b's and b's to a's

---
+++ Code_Challenges_Week_23/switcheroo.js
@@ -0,0 +1,36 @@
+/*
+
+Instructions:
+
+Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+Example:
+
+'acb' --> 'bca'
+'aabacbaa' --> 'bbabcabb'
+
+*/
+
+function switcheroo(x){
+    return x.split("").map(char => {
+      return (char === 'a') ? 'b' : (char === 'b') ? 'a' : char;
+    }).join("");
+  }
+
+/*
+
+Tests:
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Switcheroo", () => {
+  it("Fixed tests", () => {
+    assert.strictEqual(switcheroo('abc'), 'bac');
+    assert.strictEqual(switcheroo('aaabcccbaaa'), 'bbbacccabbb'); 
+    assert.strictEqual(switcheroo('ccccc'), 'ccccc');    
+  })
+});
+
+*/
