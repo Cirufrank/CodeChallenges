d3425c5c827bb1f0b2f703a253a31c4882d2afdb
Cirũ Franklin <cirufrank@me.com>
2022-09-16 10:55:19 -0700

    Add daily code challenge of function that takes in an array of numbers and returnst the highest number that occurs the most times

---
+++ Code_Challenges_Week_25/highestNumInArray.js
@@ -0,0 +1,53 @@
+/*
+
+Instructions:
+
+Complete the method which returns the number which is most frequent in the given input array. If there is a tie for most frequent number, return the largest number among them.
+
+Note: no empty arrays will be given.
+
+Examples
+[12, 10, 8, 12, 7, 6, 4, 10, 12]              -->  12
+[12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
+[12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
+
+*/
+
+function highestRank(arr){
+    const OCCURENCE_INCREMENTER = 1;
+    const ONE_OCCURENCE = 1;
+    let numOfOccurences = new Map();
+    arr.forEach(num => {
+      if (!numOfOccurences.get(num)) {
+        numOfOccurences.set(num, ONE_OCCURENCE);
+      } else {
+        const currentOccurences = numOfOccurences.get(num);
+        const totalOccurences = currentOccurences + OCCURENCE_INCREMENTER;
+        numOfOccurences.set(num, totalOccurences);
+      }
+    });
+    numOfOccurences = Array.from(numOfOccurences);
+    numOfOccurences.sort((a, b) => b[1] - a[1]);
+    const highestNumOfOccurences = numOfOccurences[0][1];
+    const highestNumsOfOccurences = numOfOccurences.filter(array => array[1] === highestNumOfOccurences);
+    if (highestNumsOfOccurences.length === 1) return highestNumsOfOccurences[0][0];
+    highestNumsOfOccurences.sort((a, b) => b[0] - a[0]);
+    return highestNumsOfOccurences[0][0];
+  }
+
+/*
+
+Tests:
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Sample tests", function() {
+  it("should test", function() {
+    var arr = [12, 10, 8, 12, 7, 6, 4, 10, 12];
+    assert.strictEqual( highestRank(arr), 12);
+  });
+});
+
+*/
