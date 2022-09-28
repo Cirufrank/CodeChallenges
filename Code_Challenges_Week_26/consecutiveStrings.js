0a12aad56dc1a383cc51c41ab2101f4d9cb0d378
Cirũ Franklin <cirufrank@me.com>
2022-09-28 10:42:51 -0700

    Add dailt code challenge of function that takes in an array of strings, creates a concatenation of k amount of strings from all the strings in the array, and returns the longest resulting string after doing so

---
+++ Code_Challenges_Week_26/consecutiveStrings.js
@@ -0,0 +1,66 @@
+/*
+
+Instructions:
+
+You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+Examples:
+strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+Concatenate the consecutive strings of strarr by 2, we get:
+
+treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+The first that came is "folingtrashy" so 
+longest_consec(strarr, 2) should return "folingtrashy".
+
+In the same way:
+longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm, "nothing" in Erlang).
+
+Note
+consecutive strings : follow one after another without an interruption
+
+ */
+
+function longestConsec(strarr, k) {
+    if (strarr.length === 0 || k > strarr.length || k <= 0) return "";
+    if (k === 1) return strarr.slice().sort((a, b) => b.length - a.length)[0];
+    let pointer = 0;
+    let largestConcatString = strarr.slice(pointer, pointer + k).join("");
+    pointer += 1;
+    while (pointer + k <= strarr.length) {
+      const currentConcatString = strarr.slice(pointer, pointer + k).join("");
+      if (largestConcatString.length < currentConcatString.length) largestConcatString = currentConcatString;
+      pointer += 1;
+    }
+  return largestConcatString;
+}
+
+/*
+
+Tests:
+
+function testing(actual, expected) {
+    Test.assertEquals(actual, expected)
+}
+
+describe("longestConsec",function() {
+it("Basic tests",function() { 
+    testing(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2), "abigailtheta")
+    testing(longestConsec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1), "oocccffuucccjjjkkkjyyyeehh")
+    testing(longestConsec([], 3), "")
+    testing(longestConsec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck")
+    testing(longestConsec(["wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"], 2), "wlwsasphmxxowiaxujylentrklctozmymu")
+    testing(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2), "")
+    testing(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3), "ixoyx3452zzzzzzzzzzzz")
+    testing(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15), "")
+    testing(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0), "")
+})})
+
+*/
