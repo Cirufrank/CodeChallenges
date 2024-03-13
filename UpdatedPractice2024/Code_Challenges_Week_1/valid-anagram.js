dcd5b089644dc8ba74d828a48f27cb8fbd9d5e34
Cirũ Franklin <cirufrank@me.com>
2024-03-13 14:45:52 -0700

    Make validAnagram test of two strings more efficient

--- UpdatedPractice2024/Code_Challenges_Week_1/valid-anagram.js
+++ UpdatedPractice2024/Code_Challenges_Week_1/valid-anagram.js
@@ -1,20 +1,48 @@
 /**
+ * 242. Valid Anagram
+Solved
+Easy
+Topics
+Companies
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+*/
+
+/**
  * @param {string} s
  * @param {string} t
  * @return {boolean}
  */
 const isAnagram = function(s, t) {
     if (s.length !== t.length) return false;
-    const charsVisited = new Map();
+    const sChars = new Map();
+    const tChars = new Map();
 
-    for (const char of s) {
-        if (charsVisited.get(char)) continue;
-        charsVisited.set(char, true);
+    for (let index = 0; index < s.length; index += 1) {
+        sChars.set(s[index], (sChars.get(s[index]) || 0) + 1)
+        tChars.set(t[index], (tChars.get(t[index]) || 0) + 1)
+    }
 
-        const totalCharsInS = s.split("").filter((sChar) => sChar === char).length
-        const totalCharsInT = t.split("").filter((tChar) => tChar === char).length
-
-        if (totalCharsInS !== totalCharsInT) return false;
+    for (const key of sChars.keys()) {
+        if (sChars.get(key) !== tChars.get(key)) return false
     }
 
     return true;
