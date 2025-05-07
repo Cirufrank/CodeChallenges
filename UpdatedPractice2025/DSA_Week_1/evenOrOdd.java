49beb5e53f17bb21781bead160d0c1cb36bf33f5
Cirũ Franklin <cirufrank@me.com>
2025-05-07 14:52:39 -0700

    Complete daily Java code challenge

---
+++ UpdatedPractice2025/DSA_Week_1/evenOrOdd.java
@@ -0,0 +1,10 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+public class Kata {
+    public static String evenOrOdd(int number) {
+        return number % 2 == 0 ? "Even" : "Odd";
+    }
+}
+
+  // Time complexity: O(1)
+  // Space complexity: O(1)
