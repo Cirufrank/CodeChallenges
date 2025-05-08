1701df537764e1e252b2916b6f10c55a8d584a0c
Cirũ Franklin <cirufrank@me.com>
2025-05-08 14:26:15 -0700

    Complete daily Golang code challenge

---
+++ UpdatedPractice2025/DSA_Week_1/multiply.go
@@ -0,0 +1,10 @@
+// This code does not execute properly. Try to figure out why.
+
+package multiply
+
+func Multiply(a, b int) int {
+  return a * b
+}
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
