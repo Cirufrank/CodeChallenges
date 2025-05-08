0feb87e80ee56e41bb4554f19410469b8be65cd1
Cirũ Franklin <cirufrank@me.com>
2025-05-08 14:24:55 -0700

    Complete daily TypeScript code challenge

---
+++ UpdatedPractice2025/DSA_Week_1/multiply.ts
@@ -0,0 +1,8 @@
+// This code does not execute properly. Try to figure out why.
+
+export function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
