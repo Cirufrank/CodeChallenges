64ed89224d874ab6991865bf86c5cc044a2aed58
Cirũ Franklin <cirufrank@me.com>
2025-05-08 14:23:20 -0700

    Complete daily Java code challenge

---
+++ UpdatedPractice2025/DSA_Week_1/multiply.java
@@ -0,0 +1,10 @@
+// This code does not execute properly. Try to figure out why.
+
+public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+}
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
