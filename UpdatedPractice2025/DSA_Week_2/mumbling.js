e2406a37d24cdd6b975da2a1194cd8c9affcf036
Cirũ Franklin <cirufrank@me.com>
2025-05-22 14:16:57 -0700

    Complete daily JS code challenge

---
+++ UpdatedPractice2025/DSA_Week_2/mumbling.js
@@ -0,0 +1,37 @@
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(s) {
+    let answer = "";
+      for (let index = 0; index < s.length; index += 1) {
+      let char = s[index]
+      if (index === 0) {
+        answer += char.toUpperCase()
+      } else {
+        answer += "-" + char.toUpperCase() + new Array(index).fill(char.toLowerCase()).join("")
+      }
+    }
+    
+    return answer;
+  }
+
+// Time complexity: O(n^2)
+// Space compleixty: O(n^2)
+
+//Tests
+
+// const Test = require('@codewars/test-compat');
+
+// describe("accum",function() {
+// it("Basic tests",function() {    
+// 	Test.assertEquals(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+// 	Test.assertEquals(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+// 	Test.assertEquals(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+// 	Test.assertEquals(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+// 	Test.assertEquals(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+// })})
